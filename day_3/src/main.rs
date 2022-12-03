22b3d70c38d73fbd108dd83550ad3584f4d89e2c
Oliver Marcusson <oliver.marcusson@elev.ga.ntig.se>
2022-12-03 14:18:52 +0100

    Klar med dag 3 part 2

--- day_3/src/main.rs
+++ day_3/src/main.rs
@@ -1,7 +1,6 @@
 use std::fs;
 
 fn find_matching_char(compartments: (&str, &str)) -> Option<char> {
-    // println!("Looking for matching character in: {:?}", compartments);
     for c1_character in compartments.0.chars() {
         for c2_character in compartments.1.chars() {
             if c1_character == c2_character {return Some(c1_character)}
@@ -11,27 +10,47 @@ fn find_matching_char(compartments: (&str, &str)) -> Option<char> {
     return None
 }
 
+fn find_badges(sacks: (&str, &str, &str)) -> Option<char> {
+    for character in sacks.0.chars() {
+        if sacks.1.find(character) != None && sacks.2.find(character) != None {
+            return Some(character);
+        }
+    }
+    println!("Couldn't find matching badges in {:?}", sacks);
+    return None;
+}
+
 fn main() {
     let priority: String = String::from("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
     let input_string = fs::read_to_string("input.txt").unwrap();
     let input_vector: Vec<&str> = input_string.split("\r\n").collect();
     let mut points: u32 = 0;
 
-    for sack in input_vector {
+    for sack in &input_vector {
         let compartments = sack.split_at((&sack.len()) / 2);
         let matching_char = find_matching_char(compartments);
 
         match matching_char {
             Some(_) => {
-                // println!("Found matching character: {}", matching_char.unwrap());
                 points += priority.find(matching_char.unwrap()).unwrap() as u32 + 1;       
             }
             None => {
                 panic!();
             } 
         }
-        // break;
     }
 
-    println!("Points: {}", points);
+    println!("Part one Points: {}", points);
+    points = 0;
+
+    for vector_index in (0..input_vector.len()).step_by(3) {
+        let badge = find_badges((&input_vector[vector_index], &input_vector[vector_index + 1], &input_vector[vector_index + 2]));
+
+        match badge {
+            Some(_) => points += priority.find(badge.unwrap()).unwrap() as u32 + 1,
+            None => panic!()
+        }
+    }
+
+    println!("Part two Points: {}", points);
 }
